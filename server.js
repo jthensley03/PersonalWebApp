46e7298b48adb6eee67b41b25f5cbe0939c14b54
jthensley03 <jasonh@udel.edu>
2024-07-10 18:31:24 -0500

    base webapp

---
+++ server.js
@@ -0,0 +1,19 @@
+// Load HTTP module
+const http = require("http");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+app.use(express.static("express"));
+
+//default url for website
+app.use("/", function(req, res){
+  res.sendFile(path.join(__dirname+'/express/index.html'));
+});
+
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
+
+console.debug(`Server listening on port ${port}`);
