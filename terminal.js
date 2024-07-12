aaa645f3cf6e2f4f35cb16ae670960deeae78a88
Jason <jasonh@udel.edu>
2024-07-11 19:58:26 -0500

    resume added, help menu functional, cmd infrastructure done

--- terminal.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("terminalInput").addEventListener("keypress", e => {
-    if (e.key === "Enter" && !e.shiftKey) {
-        e.preventDefault();
-        e.currentTarget.closest("form").submit();
-    }
-});
